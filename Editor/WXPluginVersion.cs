daf09aca4f846563d9d08f07718ffb485d870501
Ocean <oceanxiao@tencent.com>
2024-05-16 20:09:25 +0800

    Auto-publish WXSDK.

--- Editor/WXPluginVersion.cs
+++ Editor/WXPluginVersion.cs
@@ -2,7 +2,7 @@
 {
     public class WXPluginVersion
     {
-        public static string pluginVersion = "202405151937"; // 这一行不要改他，导出的时候会自动替换
+        public static string pluginVersion = "202405162007"; // 这一行不要改他，导出的时候会自动替换
     }
 
     public class WXPluginConf
