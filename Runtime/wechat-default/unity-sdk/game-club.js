ba59a6e518596c8554e7af2126a5a14b62fcd2da
nebulaliu <nebulaliu@tencent.com>
2024-09-23 16:48:16 +0800

    Auto-publish pre-release WXSDK.

--- Runtime/wechat-default/unity-sdk/game-club.js
+++ Runtime/wechat-default/unity-sdk/game-club.js
@@ -25,6 +25,10 @@ export default {
         config.type = typeEnum[config.type];
         // @ts-ignore
         config.icon = iconEnum[config.icon];
+        
+        if (!config.text) {
+            config.text = '';
+        }
         const id = uid();
         gameClubButtonList[id] = wx.createGameClubButton(config);
         return id;
