164229ecb3591cffd19f4259aadeec3dbf912f78
lidemingli <nebulaliu@tencent.com>
2026-05-29 18:13:36 +0800

    Auto-publish.

--- Editor/WXPluginVersion.cs
+++ Editor/WXPluginVersion.cs
@@ -2,7 +2,7 @@ namespace WeChatWASM
 {
     public class WXPluginVersion
     {
-        public static string pluginVersion = "202605290427"; // 这一行不要改他，导出的时候会自动替换
+        public static string pluginVersion = "202605291013"; // 这一行不要改他，导出的时候会自动替换
     }
 
     public class WXPluginConf
